9481b3996a46ed5e54231953662faf4e6c2d4f2d
Anderson Barros <anderson.barros@lansolver.com>
2018-05-25 18:31:47 -0300

    Melhorando a identação da pagina principal

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel='stylesheet' type='text/css' href='/css/app.css'>
-        <link rel='stylesheet' type='text/css' href='/css/custom.css'>
-        <title>Controle de estoque</title>
-    </head>
-    <body>
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">      
-                        <a class="navbar-brand" href="/produtos">Estoque Laravel</a>
-                    </div>
-                
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/produtos">Listagem</a></li>
-                    </ul>
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
+    <title>Controle de estoque</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">      
+                    <a class="navbar-brand" href="/produtos">Estoque Laravel</a>
                 </div>
-            </nav>
-                @yield('conteudo')
-            <footer class="footer">
-                <p>© Livro de Laravel do Alura.</p>
-            </footer>  
-        </div>
-    </body>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/produtos">Listagem</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('conteudo')
+
+        <footer class="footer">
+            <p>© Curso de Laravel do Alura.</p>
+        </footer>
+
+    </div>
+</body>
 </html>
 
 
